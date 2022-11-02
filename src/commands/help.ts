50b01aaf4abf6a31c42baa22f74f70f6154cdd51
Mini <80361877+Mini51@users.noreply.github.com>
2022-11-01 23:21:36 -0400

    Added help command

---
+++ src/commands/help.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction, EmbedBuilder} from 'discord.js'
+
+export const data = new SlashCommandBuilder().setName('help').setDescription('Responds with a list of commands')
+export function execute(interaction: CommandInteraction) {
+
+    const helpEmbed = new EmbedBuilder()
+        .setTitle('help menu')
+        .setDescription('This is a list of commands')
+        .addFields( 
+            interaction.client.commands.map(command => {
+                return {
+                    name: `**/${command.data.name}**`,
+                    value: command.data.description
+                }
+            })
+        )
+
+    return interaction.reply({embeds: [helpEmbed]})
+
+
+    
+}
